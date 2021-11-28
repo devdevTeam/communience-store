f24fff0be793cb980aea93dc27f2ae6030f91a1a
shunsuke-tamura <wwwxx4869@gmail.com>
2021-11-29 01:20:59 +0900

    add: default_cardのgetとupdateを実装

--- backend/internal/getDefaultCard.go
+++ backend/internal/getDefaultCard.go
@@ -7,20 +7,24 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func GetUserInfo(ctx *gin.Context) {
+func GetDefaultCard(ctx *gin.Context) {
 	req := ctx.Request
 	req.ParseForm()
 	uid := req.PostFormValue("uid")
-	userInfo, err := lib.SelectUser(uid)
+	cardInfo, err := lib.SelectDefaultCard(uid)
 	if err != nil {
 		ctx.Error(err)
 		return
 	}
 	resp := make(map[string]interface{})
 	resp["error"] = err
-	resp["uid"] = userInfo[0]
-	resp["mail"] = userInfo[1]
-	resp["name"] = userInfo[3]
+	resp["name"] = cardInfo[0]
+	resp["hurigana"] = cardInfo[1]
+	resp["birthday"] = cardInfo[2]
+	resp["instagram"] = cardInfo[3]
+	resp["twitter"] = cardInfo[4]
+	resp["facebook"] = cardInfo[5]
+	resp["free"] = cardInfo[6]
 	res, _ := json.Marshal(resp)
 	ctx.Writer.Write(res)
 }

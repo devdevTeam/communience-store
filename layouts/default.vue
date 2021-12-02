87d678e22403e72080f15da2ee1cf5fe852389bf
shunsuke-tamura <wwwxx4869@gmail.com>
2021-12-03 00:18:09 +0900

    add: 右上にログイン状態を表示できるようにした

--- layouts/default.vue
+++ layouts/default.vue
@@ -24,94 +24,86 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
+    <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
+        <v-icon>mdi-{{ `chevron-${miniVariant ? "right" : "left"}` }}</v-icon>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
+      <v-btn icon @click.stop="clipped = !clipped">
         <v-icon>mdi-application</v-icon>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
+      <v-btn icon @click.stop="fixed = !fixed">
         <v-icon>mdi-minus</v-icon>
       </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
+      <v-btn v-if="login_state" text @click.stop="rightDrawer = !rightDrawer">{{userInfo.name}}</v-btn>
+      <v-btn v-if="login_state" icon @click.stop="rightDrawer = !rightDrawer">
+        <v-icon large>mdi-account-circle</v-icon>
       </v-btn>
+      <v-btn v-else text to="/signin">ログイン</v-btn>
     </v-app-bar>
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
+    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
       <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        <v-list-item>
+          <v-btn outlined to="/signout">ログアウト</v-btn>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
+    <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
+import post from '@/lib/post.js'
 export default {
-  data () {
+  data() {
     return {
       clipped: false,
       drawer: false,
       fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
+          icon: "mdi-apps",
+          title: "Welcome",
+          to: "/",
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
+          icon: "mdi-chart-bubble",
+          title: "Inspire",
+          to: "/inspire",
+        },
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: "コミュスト",
+      userInfo: {
+        name: null
+      }
+    };
+  },
+  computed: {
+    login_state() {
+      if (this.$store.getters.getUser === null) {
+        return false
+      }
+      else {
+        let params = new URLSearchParams()
+        params.append("uid", this.$store.getters.getUser.uid)
+        post("/getUserInfo", params).then((res) => {
+          this.userInfo = res.data
+        })
+        return true
+      }
     }
-  }
-}
+  },
+};
 </script>
---
+++ pages/signout.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>ログアウトしています</h1>
+</template>
+
+<script>
+export default {
+  asyncData() {
+    $nuxt.$fire.auth.signOut()
+  }
+}
+</script>
